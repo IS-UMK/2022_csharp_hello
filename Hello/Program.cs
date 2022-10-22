c7f8e8894d8dc200a90ed27301fa297e65b35301
Marek <Marek@MAREK-LENOVOP53>
2022-10-22 20:23:07 +0200

    Dodaj pliki projektów.

---
+++ Hello/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imię;
+
+            Console.Write("Podaj imię: ");
+            imię = Console.ReadLine();
+
+            if (imię.Length == 0)
+            {
+                Console.WriteLine("Długość napisu wynosi zero.");
+            }
+            else
+            {
+                Console.Write("Witaj");
+                
+                int i = 0;
+                while (i < imię.Length)
+                {
+                    Console.WriteLine(imię[i]);
+                    i = i + 1;
+                }
+            }
+        }
+    }
+}
